7c409a4d5f7250d2e49d4c9c2d8e94a8a2b2516e
Frazier-Creation <ochiengfrazier@gmail.com>
2022-12-09 17:59:43 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -print alphabet in lowercase followed by a new line
+ *
+ * Return: 0
+ */
+
+int mai(void)
+{
+	char f;
+
+	f = 'a';
+	while 
+		(f <= 'z') {
+			if (( f != 'q' && f != 'e') && f <= 'z')
+				putchar(f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
