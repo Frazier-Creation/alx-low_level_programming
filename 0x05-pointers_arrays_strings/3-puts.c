7a385a339e28d5caadac331ff7a2167c18b84680
Frazier-Creation <ochiengfrazier@gmail.com>
2022-12-19 13:05:24 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include  <stdio.h>
+
+/**
+ * Main - function that prints a string
+ *
+ * Return: followed by a new line, to stdout
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
